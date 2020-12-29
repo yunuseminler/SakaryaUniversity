4e2ce7cc563bdb036f850a77eb4c227998603d9d
yunuseminler <yunus.eminler@gmail.com>
2020-12-29 23:17:44 +0300

    Veritabanı Proje Ödevi

---
+++ C# Uygulamaları/Proje1/Proje1/Form1.Designer.cs
@@ -0,0 +1,446 @@
+﻿
+namespace Proje1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.personel = new System.Windows.Forms.TextBox();
+            this.kasa = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.magaza = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.musteri = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listele = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.eski = new System.Windows.Forms.TextBox();
+            this.yeni = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.urun = new System.Windows.Forms.TextBox();
+            this.miktar = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Giris Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(331, 288);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(285, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // personel
+            // 
+            this.personel.Location = new System.Drawing.Point(114, 14);
+            this.personel.Name = "personel";
+            this.personel.Size = new System.Drawing.Size(59, 20);
+            this.personel.TabIndex = 3;
+            // 
+            // kasa
+            // 
+            this.kasa.Location = new System.Drawing.Point(114, 44);
+            this.kasa.Name = "kasa";
+            this.kasa.Size = new System.Drawing.Size(59, 20);
+            this.kasa.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PersonelNo :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "KasaNo :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "MagazaNo :";
+            // 
+            // magaza
+            // 
+            this.magaza.Location = new System.Drawing.Point(114, 81);
+            this.magaza.Name = "magaza";
+            this.magaza.Size = new System.Drawing.Size(59, 20);
+            this.magaza.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.magaza);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.personel);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.kasa);
+            this.groupBox1.Location = new System.Drawing.Point(12, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 163);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(20, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 29);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Çıkış Yap";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // musteri
+            // 
+            this.musteri.Location = new System.Drawing.Point(401, 262);
+            this.musteri.Name = "musteri";
+            this.musteri.Size = new System.Drawing.Size(59, 20);
+            this.musteri.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(328, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Musteri No : ";
+            // 
+            // listele
+            // 
+            this.listele.Location = new System.Drawing.Point(480, 258);
+            this.listele.Name = "listele";
+            this.listele.Size = new System.Drawing.Size(109, 23);
+            this.listele.TabIndex = 9;
+            this.listele.Text = "Alisveris Listele";
+            this.listele.UseVisualStyleBackColor = true;
+            this.listele.Click += new System.EventHandler(this.listele_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(635, 355);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 58);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Gün Sonu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(351, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Personel No :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(428, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(508, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Tarih : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(554, 33);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label9.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label9.Location = new System.Drawing.Point(279, 2);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(514, 439);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "  ";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(107, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Degistir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(27, 17);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Eski Musteri No : ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(27, 58);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 13);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Yeni Musteri No : ";
+            // 
+            // eski
+            // 
+            this.eski.Location = new System.Drawing.Point(123, 14);
+            this.eski.Name = "eski";
+            this.eski.Size = new System.Drawing.Size(59, 20);
+            this.eski.TabIndex = 19;
+            // 
+            // yeni
+            // 
+            this.yeni.Location = new System.Drawing.Point(123, 51);
+            this.yeni.Name = "yeni";
+            this.yeni.Size = new System.Drawing.Size(59, 20);
+            this.yeni.TabIndex = 20;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(49, 123);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 13);
+            this.label12.TabIndex = 21;
+            // 
+            // urun
+            // 
+            this.urun.Location = new System.Drawing.Point(92, 13);
+            this.urun.Name = "urun";
+            this.urun.Size = new System.Drawing.Size(59, 20);
+            this.urun.TabIndex = 10;
+            // 
+            // miktar
+            // 
+            this.miktar.Location = new System.Drawing.Point(92, 48);
+            this.miktar.Name = "miktar";
+            this.miktar.Size = new System.Drawing.Size(59, 20);
+            this.miktar.TabIndex = 22;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(23, 16);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(58, 13);
+            this.label13.TabIndex = 10;
+            this.label13.Text = "UrunKodu:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(38, 51);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(42, 13);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "Miktar :";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(50, 77);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 23);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Satis Yap";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(89, 111);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(0, 13);
+            this.label15.TabIndex = 25;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.urun);
+            this.groupBox2.Controls.Add(this.miktar);
+            this.groupBox2.Controls.Add(this.label15);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.label14);
+            this.groupBox2.Location = new System.Drawing.Point(342, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(217, 128);
+            this.groupBox2.TabIndex = 26;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ürün Satışı";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.label11);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.eski);
+            this.groupBox3.Controls.Add(this.yeni);
+            this.groupBox3.Location = new System.Drawing.Point(599, 56);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 136);
+            this.groupBox3.TabIndex = 27;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Müşteri No yenileme";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listele);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.musteri);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox personel;
+        private System.Windows.Forms.TextBox kasa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox magaza;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox musteri;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button listele;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox eski;
+        private System.Windows.Forms.TextBox yeni;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox urun;
+        private System.Windows.Forms.TextBox miktar;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ C# Uygulamaları/Proje1/Proje1/Form1.cs
@@ -0,0 +1,111 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Proje1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NpgsqlConnection baglanti = new NpgsqlConnection("server=localHost; port=5432; Database=proje; user ID=postgres; Password=1140");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            button3.Enabled = true;
+            personel.Enabled = false;
+            kasa.Enabled = false;
+            magaza.Enabled = false;
+            label9.Visible = false;
+            button1.Enabled = false;
+            label6.Text = personel.Text;
+            label8.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            NpgsqlCommand kasaAc = new NpgsqlCommand("INSERT INTO \"Kasa\" VALUES(@p1,@p2,@p3,@p4)", baglanti);
+            kasaAc.Parameters.AddWithValue("@p1", kasa.Text);
+            kasaAc.Parameters.AddWithValue("@p2", DateTime.Now.Date);
+            kasaAc.Parameters.AddWithValue("@p3", personel.Text);
+            kasaAc.Parameters.AddWithValue("@p4", magaza.Text);
+            kasaAc.ExecuteNonQuery();
+            baglanti.Close();
+        }
+
+        private void listele_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            string sorgu = "SELECT * FROM \"musterisatis\"('" + musteri.Text + "')";
+            NpgsqlDataAdapter listele = new NpgsqlDataAdapter(sorgu, baglanti);
+            DataSet ds = new DataSet();
+            listele.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            baglanti.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            string sorgu = "SELECT * FROM sonlandir('" + label8.Text + "','" + kasa.Text + "','" +magaza.Text+"')";
+            NpgsqlCommand bitir = new NpgsqlCommand(sorgu, baglanti);
+            
+            baglanti.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            button1.Enabled = true;
+            button3.Enabled = false;
+            personel.Enabled = true;
+            kasa.Enabled = true;
+            magaza.Enabled = true;
+            label9.Visible = true;
+            string sorgu = "DELETE FROM \"Kasa\" WHERE \"kasaNo\" = @p1  AND \"gunTarihi\" = @p2";
+            NpgsqlCommand kasaSil = new NpgsqlCommand(sorgu, baglanti);
+            kasaSil.Parameters.AddWithValue("@p1", kasa.Text);
+            kasaSil.Parameters.AddWithValue("@p2", DateTime.Now.Date);
+            kasaSil.ExecuteNonQuery();
+            baglanti.Close();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            string sorgu = "UPDATE \"Musteri\" SET \"musteriNo\" = @p1  WHERE \"musteriNo\" = @p2";
+            NpgsqlCommand kasaSil = new NpgsqlCommand(sorgu, baglanti);
+            kasaSil.Parameters.AddWithValue("@p1", yeni.Text);
+            kasaSil.Parameters.AddWithValue("@p2", eski.Text);
+            kasaSil.ExecuteNonQuery();
+            label12.Text = "Musteri no degistirilmiştir.";
+            yeni.Text = "";
+            eski.Text = "";
+            baglanti.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            int sayi = Convert.ToInt32(miktar.Text);
+            string sorgu = "SELECT * FROM \"satilanEkle\"(@p1,@p2)";
+            NpgsqlCommand kasaSil = new NpgsqlCommand(sorgu, baglanti);
+            kasaSil.Parameters.AddWithValue("@p1", urun.Text);
+            kasaSil.Parameters.AddWithValue("@p2", sayi);
+            kasaSil.ExecuteNonQuery();
+            label15.Text = "Urun Satisi Gerceklesmiştir";
+            urun.Text = "";
+            miktar.Text = "";
+            baglanti.Close();
+        }
+    }
+}
